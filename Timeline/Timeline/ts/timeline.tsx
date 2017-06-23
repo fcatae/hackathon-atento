9a40719d31fa065260346265abddfd226302afc8
Fabricio Catae <fcatae@microsoft.com>
2017-06-23 19:34:49 -0300

    add profile component

--- Timeline/Timeline/ts/timeline.tsx
+++ Timeline/Timeline/ts/timeline.tsx
@@ -4,4 +4,39 @@ class App extends React.Component<{}, {}> {
     }
 }
 
+class UserProfile extends React.Component<{}, {}> {
+    render() {
+        return (<div className="profile">
+                <div className="profile-pic">
+                    <img src="https://image.shutterstock.com/z/stock-photo--face-blonde-happy-bride-before-the-wedding-portrait-of-a-young-girl-with-a-beautiful-smile-the-546759520.jpg" className="profile-pic" alt="">
+                    </img>
+                    <h2>Nome cliente</h2>
+                </div>
+                <dl id="profile-info" className="inline-flex">
+                    <dt><i className="fa fa-id-card-o"></i></dt>
+                    <dd>3333.333.555-45</dd>
+                    <dt><i className="fa fa-money"></i></dt>
+                    <dd>3583,00</dd>
+                    <dt><i className="fa fa-birthday-cake"></i></dt>
+                    <dd>30</dd>
+                    <dt><i className="fa fa-map-marker"></i></dt>
+                    <dd>São Bernardo do Campo, São Paulo</dd>
+                    <dt><i className="fa fa-venus-mars"></i></dt>
+                    <dd>Masculino</dd>
+                    <dt><i className="fa fa-home"></i></dt>
+                    <dd>nada consta</dd>
+                    <dt><i className="fa fa-car"></i></dt>
+                    <dd>nada consta</dd>
+                    <dt><i className="fa fa-refresh"></i></dt>
+                    <dd>10</dd>
+                </dl>
+            </div>);
+    }
+}
+
+
+
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
+ReactDOM.render(<UserProfile />, document.getElementById('profile'));
--- Timeline/Timeline/wwwroot/js/timeline.js
+++ Timeline/Timeline/wwwroot/js/timeline.js
@@ -18,4 +18,43 @@ var App = (function (_super) {
     };
     return App;
 }(React.Component));
+var UserProfile = (function (_super) {
+    __extends(UserProfile, _super);
+    function UserProfile() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    UserProfile.prototype.render = function () {
+        return (React.createElement("div", { className: "profile" },
+            React.createElement("div", { className: "profile-pic" },
+                React.createElement("img", { src: "https://image.shutterstock.com/z/stock-photo--face-blonde-happy-bride-before-the-wedding-portrait-of-a-young-girl-with-a-beautiful-smile-the-546759520.jpg", className: "profile-pic", alt: "" }),
+                React.createElement("h2", null, "Nome cliente")),
+            React.createElement("dl", { id: "profile-info", className: "inline-flex" },
+                React.createElement("dt", null,
+                    React.createElement("i", { className: "fa fa-id-card-o" })),
+                React.createElement("dd", null, "3333.333.555-45"),
+                React.createElement("dt", null,
+                    React.createElement("i", { className: "fa fa-money" })),
+                React.createElement("dd", null, "3583,00"),
+                React.createElement("dt", null,
+                    React.createElement("i", { className: "fa fa-birthday-cake" })),
+                React.createElement("dd", null, "30"),
+                React.createElement("dt", null,
+                    React.createElement("i", { className: "fa fa-map-marker" })),
+                React.createElement("dd", null, "S\u00E3o Bernardo do Campo, S\u00E3o Paulo"),
+                React.createElement("dt", null,
+                    React.createElement("i", { className: "fa fa-venus-mars" })),
+                React.createElement("dd", null, "Masculino"),
+                React.createElement("dt", null,
+                    React.createElement("i", { className: "fa fa-home" })),
+                React.createElement("dd", null, "nada consta"),
+                React.createElement("dt", null,
+                    React.createElement("i", { className: "fa fa-car" })),
+                React.createElement("dd", null, "nada consta"),
+                React.createElement("dt", null,
+                    React.createElement("i", { className: "fa fa-refresh" })),
+                React.createElement("dd", null, "10"))));
+    };
+    return UserProfile;
+}(React.Component));
 ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
+ReactDOM.render(React.createElement(UserProfile, null), document.getElementById('profile'));
