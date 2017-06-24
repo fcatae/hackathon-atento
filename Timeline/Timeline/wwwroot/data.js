c6bd4009f52f0d3ab63a73b05aa07b7d397ffcb5
Fabricio Catae <fcatae@microsoft.com>
2017-06-23 21:05:09 -0300

    add data.js

---
+++ Timeline/Timeline/wwwroot/data.js
@@ -0,0 +1,21 @@
+
+var data = {
+
+    user: {
+        imagem: 'https://image.shutterstock.com/z/stock-photo--face-blonde-happy-bride-before-the-wedding-portrait-of-a-young-girl-with-a-beautiful-smile-the-546759520.jpg',
+        cpf: '3333.333.555-45',
+        renda: '3583,00',
+        idade: '30',
+        cidade: 'São Bernardo do Campo, SP',
+        sexo: 'Masculino',
+        imoveis: 'nada consta',
+        carros: 'nada consta',
+        tentativas: '11'
+    },
+    eventos: [ 
+        {titulo: 'CONTATO VIA SMS', data: '10/05/2017', texto: '1'},
+        {titulo: 'CONTATO VIA SMS', data: '10/05/2017', texto: '2'},
+        {titulo: 'CONTATO VIA SMS', data: '10/05/2017', texto: '3'}
+    ]
+    
+}
